220ce46f92e3a0c0abc1e4ac2cbbf1d360e1e9f9
surya <suryasarisa99@gmail.com>
2023-11-13 00:03:55 +0530

    fixed bug

--- src/pages/Lock.jsx
+++ src/pages/Lock.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+// import bg from "./peakpx.jpg";
+import bg from "./error.jpg";
+import { FaChevronRight } from "react-icons/fa";
 export default function Lock({ showError, handleSubmit, login }) {
   const navigate = useNavigate();
   useEffect(() => {
     if (login) navigate("/passes");
   }, []);
   return (
-    <div className="unlock-page">
-      <form action="" onSubmit={handleSubmit}>
-        <input type="password" name="pass" placeholder="Enter Password" />
-      </form>
+    <div className="unlock-page" style={{ backgroundmage: `url(${bg})` }}>
       {showError && <p className="error">Incorrect Password</p>}
+      <div className="box">
+        <form action="" onSubmit={handleSubmit}>
+          <input type="password" name="pass" placeholder="Enter Password" />
+          <button className="outer-btn">
+            <FaChevronRight className="icon" />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
