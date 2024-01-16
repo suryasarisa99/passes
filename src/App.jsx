ca0fa158c4f34f894f70015d404358c35956dfbf
jayasurya <suryasarisa99@gmail.com>
2024-01-16 15:40:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,31 +13,58 @@ export default function App() {
 
   useEffect(() => {
     // navigate("/unlock");
+    let permanent = JSON.parse(localStorage.getItem("permanent"));
+    if (permanent) {
+      // let url = "https://pass-backend.vercel.app/passes";
+      let url = "http://192.168.0.169:3000/passes";
+      axios
+        .post(url, { pass: "", password: "" }, { withCredentials: true })
+        .then((res) => {
+          console.log(res.data);
+          if (res.data?.permanent != undefined) {
+            localStorage.setItem("permanent", res.data.permanent);
+          }
+          if (res.data.error) {
+            setShowError(true);
+            setLogin(false);
+          } else {
+            setLogin(true);
+            setEPasses(res.data.ePasses);
+            setGPasses(res.data.gPasses);
+          }
+        });
+    }
   }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
-    let url = "https://pass-backend.vercel.app/passes";
-    // let url = "http://192.168.0.169:3000/passes";
+    // let url = "https://pass-backend.vercel.app/passes";
+    let url = "http://192.168.0.169:3000/passes";
     axios
-      .post(url, {
-        pass: e.target.pass.value,
-      })
+      .post(
+        url,
+        {
+          pass: e.target.pass.value,
+        },
+        {
+          withCredentials: true,
+        }
+      )
       .then((res) => {
-        console.log(res.data);
+        if (res.data?.permanent != undefined) {
+          localStorage.setItem("permanent", res.data.permanent);
+        }
         if (res.data.error) {
           setShowError(true);
           setLogin(false);
         } else {
           setLogin(true);
-          // navigate("/");
           setEPasses(res.data.ePasses);
           setGPasses(res.data.gPasses);
         }
       });
   }
 
-  useEffect((e) => {}, []);
   return (
     <div>
       {login ? (
